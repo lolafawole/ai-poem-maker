c0e8fdac721769c4ebfd653764489ae14c7284cc
lolafawole <30295529+lolafawole@users.noreply.github.com>
2025-10-26 20:30:13 +0000

    improving user experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function showPoem(response) {
-  let poem = document.querySelector("#poem");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -16,6 +15,8 @@ function poemGenerator(event) {
     "only write four lines and separate each line with a <br />, at the bottom sign written by Ask Athena inside a <strong> element";
   let prompt = `create a random poem about ${instructions.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poem = document.querySelector("#poem");
+  poem.innerHTML = `Generating poem about ${instructions.value}...`;
   axios.get(apiUrl).then(showPoem);
 }
 
